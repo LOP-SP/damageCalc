2850c2eb70fc4e84bb51d3631fc7a59edbc09671
Carlos Agarie <carlos.agarie@gmail.com>
2011-09-24 21:50:22 -0300

    Stored the functions for printing results in the interfaceView object.

--- src/_damagecalc.js
+++ src/_damagecalc.js
@@ -47,24 +47,14 @@ var DAMAGECALC = {
 		};
 		
 		var calcResults = function (pokemonBattle, level, value) {
-			var random = 0.85;
-
-			if (value === 'min') { random = 0.85; }
-			else { random = 1; }
-
-			var damage = Math.floor((level * 2) / 5) + 2;
-			damage = Math.floor((damage * pokemonBattle.basePower * pokemonBattle.attack) / 50);
-			damage = Math.floor((damage / pokemonBattle.defense)) + 2;
-			damage = Math.floor(damage * random);
-			damage = Math.floor(damage * pokemonBattle.stabMultiplier); 
-			damage = Math.floor(damage * pokemonBattle.effectiveness);
-
-
-			// when the minimum damage is zero, the attack deals 1 damage instead
-			if (!damage) { damage = 1; }
-
-			return damage;
+		
+		return {
+			getPokemonStats,
+			calcResults
 		};
+	})();
+	
+	var interfaceView = (function () {
 		
 		var showResultsOnUi = function (pokemonBattleResults) {
 			// Must clean the previous calculation's output
@@ -84,20 +74,12 @@ var DAMAGECALC = {
 
 			damage_table += pokemonBattleResults.minDamage_100 + " - " + pokemonBattleResults.maxDamage_100 + ")";
 
-			//damage_table += "<h2>Level 50</h2>";
-			//damage_table += 100*(pokemonBattleResults.minDamage_50 / pokemonBattleResults.hp) + "% - " + 100*(pokemonBattleResults.maxDamage_50 / pokemonBattleResults.hp) + "% (";
-			//damage_table += pokemonBattleResults.minDamage_50 + " - " + pokemonBattleResults.maxDamage_50 + ")";
-
 			damage_table += "</div>";
 
 			$("#damage").append(damage_table).show();
 		};
 		
-		return {
-			getPokemonStats,
-			calcResults,
-			showResultsOnUi
-		};
+		return showResultsOnUi;
 	})();
 	
 	//
