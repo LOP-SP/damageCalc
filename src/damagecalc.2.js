7a6d97ca10347631d3d3af127de7c6c80a2d9ca0
Carlos Agarie <carlos.agarie@gmail.com>
2011-12-28 14:00:22 -0200

    Added damageCalc 2.0 source, tests and the new indexAdded damageCalc 2.0 source, tests and the new index.

---
+++ src/damagecalc.2.js
@@ -0,0 +1,142 @@
+/*
+damageCalc
+@author: Carlos "Onox" Agarie
+@version 2.0
+Project URL: www.github.com/agarie/damageCalc
+License: MIT License
+*/
+
+"use strict";
+
+var DAMAGECALC = {};
+
+// DAMAGECALC.io is the module used to get values from the UI and to print
+// out damage tables, error messages, etc
+DAMAGECALC.io = (function () {
+}());
+
+/*
+DAMAGECALC.calculator is the module responsible for all the mathematical stuff,
+like the damage formula itself (duh), percentages and OHKO probabilities.
+*/
+DAMAGECALC.calculator = (function () {
+	return {
+		/*
+		Implementation of the damage formula.
+	
+		input: object with the following parameters:
+			level
+			basePower
+			atk
+			def
+			mod1
+			mod2
+			mod3
+			criticalHit
+			stab
+			effect
+			hasMultiscale
+		randomMultiplier: number between 0.85 and 1.00.
+	
+		Output: damage (number).
+		*/
+		damageCalc: function (input, randomMultiplier) {
+			var damage = 0;
+
+			// Damage Formula = (((((((Level × 2 ÷ 5) + 2) × BasePower × [Sp]Atk ÷ 50) ÷ [Sp]Def) × Mod1) + 2) × 
+			//                 CH × Mod2 × R ÷ 100) × STAB × Type1 × Type2 × Mod3)
+
+			// After each "step" in the damage formula, we need to round down the result.
+			damage = Math.floor(((input.level * 2) / 5) + 2);
+			damage = Math.floor(damage * input.basePower * input.atk / 50);
+			damage = Math.floor(damage / input.def);
+			damage = Math.floor(damage * input.mod1 + 2);
+			damage = Math.floor(damage * input.criticalHit);
+			damage = Math.floor(damage * input.mod2);
+
+			// This is where randomness takes place.
+			// Cache the damage before here and loop through the possible values for the random multiplier (0.85, 0.86, ..., 0.99, 1)
+			damage = Math.floor(damage * randomMultiplier);
+
+			damage = Math.floor(damage * input.stab);
+			damage = Math.floor(damage * input.effect);
+			damage = Math.floor(damage * input.mod3);
+
+			// Multiscale cuts the damage in half when active
+			damage = Math.floor(damage * input.hasMultiscale);
+
+			return damage;
+		},
+		
+		/*
+		A helper function to generate percentages.
+	
+		value: a Number object.
+
+		Output: value*100, which is a number.
+		*/
+		toPercent: function (value) {
+			return parseFloat(((value) * 10 * 10).toFixed(1));
+		},
+	
+		/*
+		Generates the probability of a pokemon OHKOing the other.
+	
+		input: object with the necessary parameters.
+		hp: foe's HP.
+	
+		Output: The probability of delivering a OHKO (between 0 and 100).
+		*/
+		ohko: function (input, hp) {
+			var prob = 0,
+			    i = 0;
+		
+			for (i = 1.0; i > 0.84; i = i - 0.01) {
+				if (this.damageCalc(input, i) >= hp) {
+					prob += (1 / 16);
+				}
+			}
+		
+			return this.toPercent(prob);
+		}
+	};
+}());
+
+// DAMAGECALC.translator is the module responsible for turning raw data from
+// the UI into an usable input object for DAMAGECALC.calculator. Also, it
+// handles the creation of damage tables.
+DAMAGECALC.translator = (function () {
+	/*
+	This module must handle all the interconnections between items, abilities
+	and whatever else. It stores items and abilities' effects in a constant.
+	
+	... How to take care of multiple cases? And the values applied (or not)?
+	
+	The raw_data object given to us from DAMAGECALC.io contains whatever was
+	selected from the UI. The items/abilities are now selected in a <select>
+	tag, not in a shitload of checkboxes anymore.
+	*/
+	
+	/*
+	// Critical hits ignores defense multipliers...
+	if (isCriticalHit === 2) {
+		damage = Math.floor( damage * stats.defStatModifier / stats.def );
+	}
+	else {
+		damage = Math.floor( damage / stats.def );
+	}
+
+	// ... and Reflect/Light Screen.
+	if (stats.isReflectActive !== 1 && isCriticalHit === 2) {
+		damage = Math.floor( ( damage * stats.mod1 * 2 ) + 2 ); 
+	}
+	else {
+		damage = Math.floor( damage * stats.mod1 + 2 );
+	}
+	*/
+	
+})();
+
+// DAMAGECALC.i18n is an internationalization module yet to be implemented.
+DAMAGECALC.i18n = (function () {
+}());
---
+++ tests/damagecalc2-test.js
@@ -0,0 +1,87 @@
+TestCase("IOTest", {
+	setUp: function() {
+		this.io = DAMAGECALC.io;
+		
+		// We'll test DOM manipulation in this test case. Prepare yourself.
+	};
+});
+
+TestCase("CalcTest", {
+	setUp: function () {
+		calc = DAMAGECALC.calculator
+		
+		this.input = {
+			level: 100,
+			basePower: 100,
+			atk: 400,
+			def: 200,
+			mod1: 1,
+			mod2: 1,
+			mod3: 1,
+			criticalHit: 1,
+			stab: 1,
+			effect: 1,
+			hasMultiscale: 1
+		};
+	},
+	
+	"test damage calc should return a number": function () {
+		var randomMultiplier = 1;
+		
+		assertNotNaN(calc.damageCalc(this.input, randomMultiplier));
+		assertNumber(calc.damageCalc(this.input, randomMultiplier));
+	},
+	
+	"test damage calc should return correct min and max values": function () {
+		var min = 0.85,
+		    max = 1.0;
+		
+		assertEquals(144, calc.damageCalc(this.input, min));
+		assertEquals(170, calc.damageCalc(this.input, max));
+	},
+	
+	"test toPercent should return number": function () {
+		var den = 255,
+		    num = 711;
+		
+		assertNotNaN(calc.toPercent(den / num));
+		assertNumber(calc.toPercent(den / num));
+	},
+	
+	"test toPercent should return correct value": function () {
+		var den = 10,
+		    num = 100;
+		
+		assertEquals(10, calc.toPercent(den / num));
+	},
+	
+	"test ohko should return a number": function () {
+		var hp = 333;
+		
+		assertNotNaN(calc.ohko(this.input, hp));
+		assertNumber(calc.ohko(this.input, hp));
+	},
+	
+	"test ohko should return a value between 0 and 100": function () {
+		var hp = 437;
+		
+		assertTrue(calc.ohko(this.input, hp) <= 100 && calc.ohko(this.input, hp) >= 0);
+	},
+	
+	"test ohko should return correct value": function () {
+		var hp = 256;
+		
+		assertEquals(0, calc.ohko(this.input, hp));
+		
+		this.input.atk = 378;
+		this.input.def = 127;
+		this.input.basePower = 102;
+		this.input.stab = 1.5;
+		
+		assertEquals(100, calc.ohko(this.input, hp));
+	}
+});
+
+TestCase("TranslatorTest", {
+	
+});
